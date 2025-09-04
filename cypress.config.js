e3f4047a03282170c87e31f2a404a3ac7eed68aa
Trisha May Debil <216677563+tdebil-tlc@users.noreply.github.com>
2025-09-04 23:46:29 +0800

    add report

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
-const { defineConfig } = require("cypress");
+
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
@@ -6,4 +7,12 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
-});
+
+  reporter: 'mochawesome',
+  reporterOptions: {
+    // reportDir: 'cypress/results',
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+})
